928f56aa8bd4aba75e2da746361e8f868544e4f8
Gary SUBLET <gary.sublet@hotmail.fr>
2020-06-03 21:28:09 +0200

    spawn vifm

--- config.h
+++ config.h
@@ -63,11 +63,13 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "rofi", "-show", "run", NULL };
 /* static const char *termcmd[]  = { "alacritty", NULL }; */
 static const char *termcmd[]  = { "sh", "-c", "cd `xcwd`; st", NULL };
+static const char *vifmcmd[]  = { "sh", "-c", "cd `xcwd`; st -e vifm `pwd`", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY|ShiftMask,             XK_w,      spawn,          {.v = vifmcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
