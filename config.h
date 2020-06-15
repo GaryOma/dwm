4b5bd9fee5ac0a18025e1771292d03800609b7c4
Gary SUBLET <gary.sublet@hotmail.fr>
2020-06-15 08:35:27 +0200

    change keybindings for multiple moitor setup

--- config.h
+++ config.h
@@ -130,9 +130,9 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
+	{ MODKEY,                       XK_q,      focusmon,       {.i = -1 } },
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_q,      tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
     /* gaps management */
 	{ MODKEY|ControlMask,           XK_l,      incrgaps,       {.i = +10 } },
